b83f0a8458e163828b0787b659c7174ea8b31767
Nicolò Pasini <nicolo.pasini@edreamsodigeo.com>
2024-09-21 13:07:27 +0200

    exercise 1 - implement mocked network monitor for testing

---
+++ networkPerformer.playground/Sources/MockedNetworkMonitor.swift
@@ -0,0 +1,36 @@
+import Foundation
+
+public class NetworkMonitorMock: NetworkMonitorProtocol {
+    public init() {}
+    
+    public func networkAvailabilityStream() -> AsyncStream<Bool> {
+        AsyncStream<Bool> { continuation in
+            Task {
+                for await value in BoolGenerator() {
+                    continuation.yield(value)
+                }
+                
+                continuation.finish()
+            }
+        }
+    }
+    
+    public func isInternetConnectionAvailable() -> Bool {
+        false
+    }
+}
+
+fileprivate struct BoolGenerator: AsyncSequence, AsyncIteratorProtocol {
+    typealias Element = Bool
+    private var counter = 0
+
+    mutating func next() async -> Element? {
+        try? await Task.sleep(nanoseconds: 500_000_000)
+        counter += 1
+        return counter == 3
+    }
+
+    func makeAsyncIterator() -> BoolGenerator {
+        self
+    }
+}

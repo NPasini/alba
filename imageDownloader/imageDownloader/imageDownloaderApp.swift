da657e6e93fd90ffb21ac556ace9f26de7dcc103
Nicolò Pasini <nicolo.pasini@edreamsodigeo.com>
2024-09-22 10:03:50 +0200

    exercise 2 - initialized app project

---
+++ imageDownloader/imageDownloader/imageDownloaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  imageDownloaderApp.swift
+//  imageDownloader
+//
+//  Created by nicolo.pasini on 22/09/24.
+//
+
+import SwiftUI
+
+@main
+struct imageDownloaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

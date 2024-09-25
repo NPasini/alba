e11dbcaa42bd3c442f73f3abde99c0bafd29ed3e
Nicolò Pasini <nicolo.pasini@edreamsodigeo.com>
2024-09-25 13:20:06 +0200

    exercise 2 - add mocked network monitors

---
+++ imageDownloader/imageDownloader/Utils/Mocks/NetworkMonitorMock.swift
@@ -0,0 +1,80 @@
+//
+//  NetworkMonitorMock.swift
+//  imageDownloader
+//
+//  Created by nicolo.pasini on 25/09/24.
+//
+
+import Foundation
+
+public enum NetworkMonitorMock {
+    public static let available: BaseNetworkMonitorMock = AvailableNetworkMonitorMock()
+    public static let neverAvailable: BaseNetworkMonitorMock = NeverAvailableNetworkMonitorMock()
+    public static let initiallyNotAvailable: BaseNetworkMonitorMock = InitiallyNotAvailableNetworkMonitorMock()
+}
+
+public class BaseNetworkMonitorMock: NetworkMonitorProtocol {
+    private let networkInitiallyAvailable: Bool
+    private let networkNotAvailableStreamLimit: Int
+    
+    public init(networkInitiallyAvailable: Bool, networkNotAvailableStreamLimit: Int = 0) {
+        self.networkInitiallyAvailable = networkInitiallyAvailable
+        self.networkNotAvailableStreamLimit = networkNotAvailableStreamLimit
+    }
+    
+    public func isInternetConnectionAvailable() -> Bool {
+        networkInitiallyAvailable
+    }
+    
+    public func networkAvailabilityStream() -> AsyncStream<Bool> {
+        AsyncStream { continuation in
+            Task {
+                for await value in BoolGenerator(limit: networkNotAvailableStreamLimit) {
+                    continuation.yield(value)
+                }
+                
+                continuation.finish()
+            }
+        }
+    }
+}
+
+fileprivate final class AvailableNetworkMonitorMock: BaseNetworkMonitorMock {
+    init() {
+        super.init(networkInitiallyAvailable: true)
+    }
+}
+
+fileprivate class NeverAvailableNetworkMonitorMock: BaseNetworkMonitorMock {
+    init() {
+        super.init(networkInitiallyAvailable: false, networkNotAvailableStreamLimit: 50)
+    }
+}
+
+fileprivate class InitiallyNotAvailableNetworkMonitorMock: BaseNetworkMonitorMock {
+    init() {
+        super.init(networkInitiallyAvailable: false, networkNotAvailableStreamLimit: 3)
+    }
+}
+
+fileprivate struct BoolGenerator: AsyncSequence, AsyncIteratorProtocol {
+    typealias Element = Bool
+
+    private let limit: Int
+    private var counter = 0
+
+    init(limit: Int) {
+        self.limit = limit
+    }
+
+    mutating func next() async -> Element? {
+        try? await Task.sleep(nanoseconds: 500_000_000)
+        counter += 1
+        return counter >= limit
+    }
+
+    func makeAsyncIterator() -> BoolGenerator {
+        self
+    }
+}
+

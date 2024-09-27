6a37a45f8a40d753a029a0f429d7f3fa804c256f
Nicolò Pasini <nicolo.pasini@edreamsodigeo.com>
2024-09-27 19:31:34 +0200

    exercise 2 - passing two different instances of network monitor in composition

--- imageDownloader/imageDownloader/imageDownloaderApp.swift
+++ imageDownloader/imageDownloader/imageDownloaderApp.swift
@@ -11,15 +11,14 @@ import SwiftUI
 struct imageDownloaderApp: App {
     private let router = Router()
     private let httpClient = URLSessionHTTPClient()
-    private let networkMonitor = NWNetworkMonitor()
     
     var body: some Scene {
         WindowGroup {
             ScreensStackView(
                 model: LoadingViewModel(
                     httpClient: httpClient, 
-                    networkMonitor: networkMonitor,
-                    networkPerformer: NetworkOperationPerformer(networkMonitor: networkMonitor),
+                    networkMonitor: NWNetworkMonitor(),
+                    networkPerformer: NetworkOperationPerformer(networkMonitor: NWNetworkMonitor()),
                     onDownloadCompleted: { data in
                         router.navigate(to: .imageScreen(imageData: data))
                     }
